4083e049621676561a33aa4035df0130d2e2dcfe
prajwal.bandak <prajwal.bandak777@gmail.com>
2023-10-27 21:23:32 +0530

    MonogDB connection is succesfull.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion }  from 'mongodb';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO,
+       {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      
+    }
+  ).then(() => {
+    console.log("MongoDB is connected");
+  }).catch((error) => {
+    console.error("MongoDB connection error: ", error);
+  });
+  
 const app = express();
+console.log(process.env.MONGO)
 
 app.listen(3000, ()=>{
     console.log("server is started");
