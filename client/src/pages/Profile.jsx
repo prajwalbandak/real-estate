4aaa61421902aab28e00b2c8c76232397b52946b
prajwal.bandak <prajwal.bandak777@gmail.com>
2023-11-04 16:20:30 +0530

    Added the goggle authentication provider to singing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,80 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../../firebase';
 
 const Profile = () => {
   //const currentUser = useSelector((state) => state.user);
   const { currentUser} = useSelector(state=> state.user);
+  const [file, setFile] = useState(undefined);
+  const [ formData, setFormData] = useState({});
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+  const fileref = useRef(null);
   //console.log(currentUser);
-  console.log(currentUser.rest.avatar);
+
+  console.log("form data " + formData);
+  console.log("file percentage "  + filePerc);
+  console.log("errorHandere " + fileUploadError);
+  useEffect (() =>{
+    
+    if(file){
+      hanadleFileUpload(file);
+    }
+  },[file])
+
+  const hanadleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const StorageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(StorageRef, file);
+  
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setFilePerc(Math.round(progress));
+        },
+        (error) => {
+          setFileUploadError(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+            setFormData({ ...formData, avatar: downloadURL })
+          );
+        }
+      );
+    };
+
+
   
   return (
 
     <div className='p-3 max-w-lg mx-auto gap-4'>
       <h1 className='text-3xl text-center py-7 font-semibold '> Profile</h1>
       <form className='flex flex-col gap-3'> 
-        <img className='h-24 w-24  cursor-pointer rounded-full self-center object-cover'src={currentUser.rest.avatar} alt='profile' />
+      <form className='flex flex-col gap-3'> 
+      <input  ref={fileref} onChange= {(e) => setFile(e.target.files[0])} type="file" hidden accept='images/*'></input>
+      
+       
+        <img 
+              onClick= {() => {fileref.current.click() }}
+              className='h-24 w-24  cursor-pointer rounded-full self-center object-cover'src={currentUser.rest.avatar} alt='profile' />
+          </form>
         <input type='text' placeholder='userName' id='username'
         className='rounded-lg p-3 border gap-2'></input> 
+      
+
         <input type='text' placeholder='email' id='email' className='rounded-lg p-3 border gap-2'></input> 
         <input type='password' placeholder='password' id='password' className='rounded-lg p-3 border gap-2'></input> 
         <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-70 p-3' >update</button>
