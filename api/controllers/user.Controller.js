ad12dfb3740f62b1059343d7a64f11ab0282993d
prajwal.bandak <prajwal.bandak777@gmail.com>
2023-11-06 20:57:59 +0530

    Added the updated Router API

--- api/controllers/user.Controller.js
+++ api/controllers/user.Controller.js
@@ -1,8 +1,40 @@
+import User from "../models/User.model.js";
+import errorHandler from "../utils/error.js"
+import bcrypt from 'bcryptjs'
 
 
 
+export const updateUser = (req, res, next) =>{
 
-export const updateUser = () =>{
-    
+    if(req.user.id != req.param.id) {
+        return next(errorHandler("401", "You have have to update your own account"));
+    }
+    try{
+        if(req.body.password){
+            const hashPassword = bcrypt.hashSync(req.body.password, 10);
+
+        }
+        const updatedUser = User.findByIdAndUpdate(
+            req.param.id,
+            {
+                $set :{
+                    username:req.body.username,
+                    email:req.body.email,
+                    password:req.body.password,
+                    avatar: req.body.avatar,
+                },
+            },
+            {new:true}
+        )
+        const { password:pass, ...rest} = updatedUser._doc;
+        res.status(201).json({
+            rest
+        })
+
+    }catch(error){
+        next(error);
+    }
+
+
 }
 
